9b0933d4908d7220006cec41a4e169526d54c29b
Robert Heinzmann <reg@elconas.de>
2016-11-18 14:19:14 +0100

    Allow Workaround for https://github.com/jenkinsci/puppet-jenkins/issues/665

--- manifests/init.pp
+++ manifests/init.pp
@@ -173,6 +173,9 @@
 #
 # group = 'jenkins' (default)
 #
+# default_plugins = [ 'credentials' ] (default)
+#   List of default plugins to manage, overwrite if you manage
+#   all plugins
 #
 class jenkins(
   $version            = $jenkins::params::version,
@@ -211,6 +214,7 @@ class jenkins(
   $user               = $::jenkins::params::user,
   $manage_group       = $::jenkins::params::manage_group,
   $group              = $::jenkins::params::group,
+  $default_plugins    = $::jenkins::params::default_plugins,
 ) inherits jenkins::params {
 
   validate_string($version)
@@ -290,7 +294,7 @@ class jenkins(
     include jenkins::service
     validate_array($default_plugins)
     if empty($default_plugins){
-      notice(sprintf('INFO: make sure you install the following plugins with your code using this module: %s',join($::jenkins::params::default_plugins,',')))
+      notice(sprintf("INFO: make sure you install the following plugins with your code using this module: %s",join($::jenkins::params::default_plugins,',')))
     }
   }
 
